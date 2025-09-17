c300b13b497360c3d04c515ccb05132e91df4f62
Yusufkckk <yusufkocak3469@gmail.com>
2025-09-17 11:45:50 +0300

    Veritabanı bağlantısı ve DbContext sınıfı eklend

---
+++ GiyimMagazasi/GiyimMagazasi.Server/Data/UygulamaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GiyimMagazasi.Server.Data
+{
+    public class UygulamaDbContext : DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options)
+        {
+        }
+
+        // Modellerimizi buraya ekleyeceğiz.
+        // public DbSet<Product> Products { get; set; }
+    }
+}
